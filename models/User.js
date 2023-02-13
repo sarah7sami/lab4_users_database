f3eff265365ebb7bd761af9716e5faf1cad409e7
Sarah Sami <sarah7sami@gmail.com>
2023-02-12 20:03:14 -0500

    Update model for email validation to be more permissive

--- models/User.js
+++ models/User.js
@@ -10,27 +10,61 @@ const userSchema = new schema({
   email: {
     type: String,
     required: true,
-    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    match: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
   },
-  city: {
+  address: {
+    street: {
+      type: String,
+      required: true
+    },
+    suite: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true,
+      match: /^[a-zA-Z ]+$/
+    },
+    zipcode: {
+      type: String,
+      required: true,
+      match: /^\d{5}-\d{4}$/
+    },
+    geo: {
+      lat: {
+        type: String,
+        required: true
+      },
+      lng: {
+        type: String,
+        required: true
+      }
+    }
+  },
+  phone: {
     type: String,
     required: true,
-    match: /^[a-zA-Z ]+$/
+    match: /^\d{1}-\d{3}-\d{3}-\d{4}$/
   },
   website: {
     type: String,
     required: true,
     match: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
   },
-  zip: {
-    type: String,
-    required: true,
-    match: /^\d{5}-\d{4}$/
-  },
-  phone: {
-    type: String,
-    required: true,
-    match: /^\d{1}-\d{3}-\d{3}-\d{4}$/
+  company: {
+    name: {
+      type: String,
+      required: true
+    },
+    catchPhrase: {
+      type: String,
+      required: true
+    },
+    bs: {
+      type: String,
+      required: true
+    }
   }
 });
 
